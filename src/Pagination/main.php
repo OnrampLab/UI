077e306d442cc30d021098f009612a8cb47bf10a
root <root@training3.training3>
2015-08-14 01:34:51 -0700

    update

--- src/Pagination/main.php
+++ src/Pagination/main.php
@@ -1,10 +1,34 @@
 
-    <div id="content"></div>
-
     <script type="text/jsx" src="src/Pagination/pagination.jsx"></script>
     <script type="text/jsx" src="src/Pagination/main.js"></script>
 
+    <div class="row">
+        <div class="col-md-10">
+
+            <div id="content"></div>
+            <?php output(); ?>
+
+        </div>
+        <div class="col-md-2">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">相依性</h3>
+                </div>
+                <div class="list-group">
+                    <a class="list-group-item" href="javascript:void(0);">Bootstrap</a>
+                    <a class="list-group-item" href="javascript:void(0);">未使用 jQuery</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
 <?php
+function output() {
+
 $output = <<<'EOD'
 <Pagination {...this.state} handlePage={this.page} />
 
@@ -30,5 +54,7 @@ Example:
     }
 EOD;
 outputCode($output);
+
+}
 ?>
 
